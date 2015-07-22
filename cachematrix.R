4e64a9e95667bef125718e417d2e4c54735d6706
Ritesh Tandon <tandon.ritz@gmail.com>
2015-07-22 23:26:09 +0530

    Adding cachematrix.R and ReadMe file

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,47 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function takes a matrix as argument and returns a list of four functions that can be applied on the matrix
+##set the value of the matrix
+##get the value of the matrix
+##set the inverse of matrix
+##get the inverse of matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mymatrix = matrix()) {
+  myinverse <- NULL
+  set <- function(y) {
+    mymatrix <<- y
+    myinverse <<- NULL
+  }
+  get <- function() mymatrix
+  setInverse <- function(inverse) myinverse <<- inverse
+  getInverse <- function() myinverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The following function compute the inverse of matrix created with the above function. 
+##it first checks to see if the inverse has already been calculated. 
+##If so, it gets the inverse from the cache and skips the computation. Otherwise, 
+##it calculates the inverse of the matrix and sets it in the cache via the setInverse function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mymatrix, ...) {
+  myinverse <- mymatrix$getInverse()
+  if(!is.null(myinverse)) {
+    message("getting cached data")
+    return(myinverse)
+  }
+  data <- mymatrix$get()
+  myinverse <- solve(data)
+  mymatrix$setInverse(myinverse)
+  myinverse
 }
+
+
+## Above function can be tested using following. If the product of matrix x and the inverse matrix
+## reurned by cacheSolve function is identity atrix , then the answer is correct.
+
+##x<-makeCacheMatrix(matrix(c(4,3,3,2),nrow=2,ncol=2))
+##cacheSolve(x)
